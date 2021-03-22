fe7bb206f99314be57f6b13fb7a3f503121fbd99
Christopher Shipstone <christopher.shipstone98@gmail.com>
2021-03-22 10:25:43 +0000

    Added constructor and Item property

--- Shipstone.System/Collections/FrequencyTable.cs
+++ Shipstone.System/Collections/FrequencyTable.cs
@@ -4,27 +4,160 @@ using System.Collections.Generic;
 
 namespace Shipstone.System.Collections
 {
+    /// <summary>
+    /// Represents a strongly types frequency table of items.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the list.</typeparam>
     public partial class FrequencyTable<T> : ICollection, ICollection<T>, IReadOnlyCollection<T>
     {
-        public int Count => throw new NotImplementedException();
-        public IEnumerable<int> Frequencies => throw new NotImplementedException();
+        private int _Count;
+        private readonly IList<int> _Frequencies;
+        private readonly IList<T> _Items;
+
+#region Properties
+        /// <summary>
+        /// Gets the number of items contained in the <see cref="FrequencyTable{T}" />.
+        /// </summary>
+        /// <value>The number of items contained in the <see cref="FrequencyTable{T}" />.</value>
+        public int Count => this._Count;
+
+        /// <summary>
+        /// Gets a collection containing the frequencies of all items contained in the <see cref="FrequencyTable{T}" />.
+        /// </summary>
+        /// <value>A collection containing the frequencies of all items contained in the <see cref="FrequencyTable{T}" />.</value>
+        public IEnumerable<int> Frequencies => this._Frequencies;
+
         bool ICollection<T>.IsReadOnly => false;
         bool ICollection.IsSynchronized => false;
-        public IEnumerable<T> Items => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gets a collection containing all items contained in the <see cref="FrequencyTable{T}" />.
+        /// </summary>
+        /// <value>A collection containing all items contained in the <see cref="FrequencyTable{T}" />.</value>
+        public IEnumerable<T> Items => this._Items;
+
         public int MaxFrequency => throw new NotImplementedException();
         public int MinFrequency => throw new NotImplementedException();
         Object ICollection.SyncRoot => this;
 
+        /// <summary>
+        /// Gets or sets the frequency of the specified item.
+        /// </summary>
+        /// <param name="item">The item of the frequency to get or set.</param>
+        /// <value>The frequency of the specified item.</value>
+        /// <exception cref="ArgumentOutOfRangeException">On a set operation, value is less than 0.</exception>
         public int this[T item]
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get
+            {
+                int index = this._Items.IndexOf(item);
+                return index == -1 ? 0 : this._Frequencies[index];
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+
+                int index = this._Items.IndexOf(item);
+
+                if (index == -1)
+                {
+                    if (value == 0)
+                    {
+                        return;
+                    }
+
+                    this._Items.Add(item);
+                    this._Frequencies.Add(value);
+                    this._Count += value;
+                }
+                
+                else
+                {
+                    int frequency = this._Frequencies[index];
+
+                    if (value == 0)
+                    {
+                        this._Items.RemoveAt(index);
+                        this._Frequencies.RemoveAt(index);
+                        this._Count -= frequency;
+                    }
+
+                    else
+                    {
+                        this._Frequencies[index] = value;
+                        this._Count += value - frequency;
+                    }
+                }
+            }
+        }
+#endregion
+
+#region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrequencyTable{T}" /> class that is empty.
+        /// </summary>
+        public FrequencyTable()
+        {
+            this._Frequencies = new List<int>();
+            this._Items = new List<T>();
         }
 
-        public FrequencyTable() => throw new NotImplementedException();
-        public FrequencyTable(IEnumerable<T> collection) => throw new NotImplementedException();
-        public FrequencyTable(FrequencyTable<T> table) => throw new NotImplementedException();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrequencyTable{T}" /> class that contains items copied from the specified collection.
+        /// </summary>
+        /// <param name="collection">The collection whose items are copied to the new frequency table.</param>
+        /// <exception cref="ArgumentNullException"><c><paramref name="collection" /></c> is <c>null</c>.</exception>
+        public FrequencyTable(IEnumerable<T> collection) : this()
+        {
+            foreach (T item in collection ?? throw new ArgumentNullException(nameof (collection)))
+            {
+                this._Add(item, 1);
+            }
+        }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrequencyTable{T}" /> class that contains items copied from the specified frequency table.
+        /// </summary>
+        /// <param name="table">The source table whose items are copied to the new frequency table.</param>
+        /// <exception cref="ArgumentNullException"><c><paramref name="table" /></c> is <c>null</c>.</exception>
+        public FrequencyTable(FrequencyTable<T> table)
+        {
+            if (table is null)
+            {
+                throw new ArgumentNullException(nameof (table));
+            }
+
+            this._Count = table._Count;
+            this._Frequencies = new List<int>(table._Frequencies);
+            this._Items = new List<T>(table._Items);
+        }
+#endregion
+
+#region Private methods
+        private void _Add(T item, int frequency)
+        {
+            int index = this._Items.IndexOf(item);
+
+            if (index == -1)
+            {
+                this._Items.Add(item);
+                this._Frequencies.Add(frequency);
+            }
+
+            else
+            {
+                this._Frequencies[index] += frequency;
+            }
+
+            this._Count += frequency;
+        }
+#endregion
+
+#region Public methods
         public void Add(T item) => throw new NotImplementedException();
         public void Add(T item, int frequency) => throw new NotImplementedException();
         public void AddRange(IEnumerable<T> collection) => throw new NotImplementedException();
@@ -68,5 +201,6 @@ namespace Shipstone.System.Collections
         public void ToArray()  => throw new NotImplementedException();
         public void ToArray(int frequency) => throw new NotImplementedException();
         public void ToArray(int minFrequency, int maxFrequency) => throw new NotImplementedException();
+#endregion
     }
 }
--- Shipstone.SystemTest/FrequencyTableTest.cs
+++ Shipstone.SystemTest/FrequencyTableTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Shipstone.System.Collections;
@@ -8,5 +10,226 @@ namespace Shipstone.SystemTest
     [TestClass]
     public class FrequencyTableTest
     {
+        private const int _SampleLength = 40;
+
+        private static readonly Random _Random;
+
+        private FrequencyTable<int> _Table;
+
+        static FrequencyTableTest() => FrequencyTableTest._Random = new Random();
+
+        private static int[] _Sample() => FrequencyTableTest._Sample(0, Int32.MaxValue);
+        private static int[] _Sample(out Dictionary<int, int> dictionary) => FrequencyTableTest._Sample(0, Int32.MaxValue, out dictionary);
+        private static int[] _Sample(int min, int max) => FrequencyTableTest._Sample(min, max, out Dictionary<int, int> dictionary);
+
+        private static int[] _Sample(int min, int max, out Dictionary<int, int> dictionary)
+        {
+            int[] sample = new int[FrequencyTableTest._SampleLength];
+            dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < sample.Length; i ++)
+            {
+                int n = FrequencyTableTest._Random.Next(min, max);
+                sample[i] = n;
+                
+                if (dictionary.ContainsKey(n))
+                {
+                    ++ dictionary[n];
+                }
+
+                else
+                {
+                    dictionary.Add(n, 1);
+                }
+            }
+
+            return sample;
+        }
+
+        [TestInitialize]
+        public void Initialize() => this._Table = new FrequencyTable<int>();
+
+#region Item tests
+        public void TestItem_InvalidRange()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => this._Table[10] = Int32.MinValue);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => this._Table[10] = -1);
+        }
+
+        [TestMethod]
+        public void TestItem_NonZeroToNonZero_MultipleItems()
+        {
+            const int CONTROL = 10, CONSTANT = CONTROL * 2;
+            this._Table[CONTROL] = CONTROL;
+            this._Table[CONSTANT] = CONSTANT;
+            this._Table[CONTROL] = CONSTANT;
+            Assert.AreEqual(CONSTANT * 2, this._Table.Count);
+            Assert.AreEqual(2, this._Table.Frequencies.Count());
+            Assert.AreEqual(2, this._Table.Items.Count());
+            Assert.IsTrue(this._Table.Items.Contains(CONTROL));
+            Assert.IsTrue(this._Table.Items.Contains(CONSTANT));
+            Assert.AreEqual(CONSTANT, this._Table[CONTROL]);
+            Assert.AreEqual(CONSTANT, this._Table[CONSTANT]);
+        }
+
+        [TestMethod]
+        public void TestItem_NonZeroToZero_MultipleItems()
+        {
+            const int CONTROL = 10, CONSTANT = CONTROL * 2;
+            this._Table[CONTROL] = CONTROL;
+            this._Table[CONSTANT] = CONSTANT;
+            this._Table[CONTROL] = 0;
+            Assert.AreEqual(CONSTANT, this._Table.Count);
+            Assert.AreEqual(1, this._Table.Frequencies.Count());
+            Assert.AreEqual(1, this._Table.Items.Count());
+            Assert.IsFalse(this._Table.Items.Contains(CONTROL));
+            Assert.IsTrue(this._Table.Items.Contains(CONSTANT));
+            Assert.AreEqual(0, this._Table[CONTROL]);
+            Assert.AreEqual(CONSTANT, this._Table[CONSTANT]);
+        }
+
+        [TestMethod]
+        public void TestItem_ZeroToNonZero_MultipleItems()
+        {
+            const int CONTROL = 10, CONSTANT = CONTROL * 2;
+            this._Table[CONTROL] = CONTROL;
+            this._Table[CONSTANT] = CONSTANT;
+            Assert.AreEqual(CONTROL + CONSTANT, this._Table.Count);
+            Assert.AreEqual(2, this._Table.Frequencies.Count());
+            Assert.AreEqual(2, this._Table.Items.Count());
+            Assert.IsTrue(this._Table.Items.Contains(CONTROL));
+            Assert.IsTrue(this._Table.Items.Contains(CONSTANT));
+            Assert.AreEqual(CONTROL, this._Table[CONTROL]);
+            Assert.AreEqual(CONSTANT, this._Table[CONSTANT]);
+        }
+
+        [TestMethod]
+        public void TestItem_ZeroToZero_MultipleItems()
+        {
+            const int CONTROL = 10, CONSTANT = CONTROL * 2;
+            this._Table[CONTROL] = 0;
+            this._Table[CONSTANT] = CONSTANT;
+            Assert.AreEqual(CONSTANT, this._Table.Count);
+            Assert.AreEqual(1, this._Table.Frequencies.Count());
+            Assert.AreEqual(1, this._Table.Items.Count());
+            Assert.IsFalse(this._Table.Items.Contains(CONTROL));
+            Assert.IsTrue(this._Table.Items.Contains(CONSTANT));
+            Assert.AreEqual(0, this._Table[CONTROL]);
+            Assert.AreEqual(CONSTANT, this._Table[CONSTANT]);
+        }
+
+        [TestMethod]
+        public void TestItem_NonZeroToNonZero_SingleItem()
+        {
+            const int ITEM = 10, FREQ_1 = ITEM, FREQ_2 = ITEM * 2;
+            this._Table[ITEM] = FREQ_1;
+            this._Table[ITEM] = FREQ_2;
+            Assert.AreEqual(FREQ_2, this._Table.Count);
+            Assert.AreEqual(1, this._Table.Frequencies.Count());
+            Assert.AreEqual(1, this._Table.Items.Count());
+            Assert.IsTrue(this._Table.Items.Contains(ITEM));
+            Assert.AreEqual(FREQ_2, this._Table[ITEM]);
+        }
+
+        [TestMethod]
+        public void TestItem_NonZeroToZero_SingleItem()
+        {
+            const int ITEM_1 = 10;
+            this._Table[ITEM_1] = ITEM_1;
+            this._Table[ITEM_1] = 0;
+            Assert.AreEqual(0, this._Table.Count);
+            Assert.AreEqual(0, this._Table.Frequencies.Count());
+            Assert.AreEqual(0, this._Table.Items.Count());
+            Assert.IsFalse(this._Table.Items.Contains(ITEM_1));
+            Assert.AreEqual(0, this._Table[ITEM_1]);
+        }
+
+        [TestMethod]
+        public void TestItem_ZeroToNonZero_SingleItem()
+        {
+            const int ITEM = 10;
+            this._Table[ITEM] = ITEM;
+            Assert.AreEqual(ITEM, this._Table.Count);
+            Assert.AreEqual(1, this._Table.Frequencies.Count());
+            Assert.AreEqual(1, this._Table.Items.Count());
+            Assert.IsTrue(this._Table.Items.Contains(ITEM));
+            Assert.AreEqual(ITEM, this._Table[ITEM]);
+        }
+
+        [TestMethod]
+        public void TestItem_ZeroToZero_SingleItem()
+        {
+            const int ITEM = 10;
+            this._Table[ITEM] = 0;
+            Assert.AreEqual(0, this._Table.Count);
+            Assert.AreEqual(0, this._Table.Frequencies.Count());
+            Assert.AreEqual(0, this._Table.Items.Count());
+            Assert.IsFalse(this._Table.Items.Contains(ITEM));
+            Assert.AreEqual(0, this._Table[ITEM]);
+        }
+#endregion
+
+#region Constructor tests
+        [TestMethod]
+        public void TestConstructor_IEnumerable_Empty()
+        {
+            FrequencyTable<int> table = new FrequencyTable<int>(Array.Empty<int>());
+            Assert.AreEqual(0, table.Count);
+            Assert.AreEqual(0, table.Frequencies.Count());
+            Assert.AreEqual(0, table.Items.Count());
+        }
+
+        [TestMethod]
+        public void TestConstructor_IEnumerable_NotEmpty()
+        {
+            int[] sample = FrequencyTableTest._Sample(1, 10, out Dictionary<int, int> dictionary);
+            FrequencyTable<int> table = new FrequencyTable<int>(sample);
+            Assert.AreEqual(sample.Length, table.Count);
+
+            for (int i = 0; i < sample.Length; i ++)
+            {
+                int n = sample[i];
+                Assert.IsTrue(table.Items.Contains(n));
+                Assert.AreEqual(dictionary[n], table[n]);
+            }
+        }
+
+        [TestMethod]
+        public void TestConstructor_IEnumerable_Null() => Assert.ThrowsException<ArgumentNullException>(() => new FrequencyTable<int>(null as int[]));
+
+        [TestMethod]
+        public void TestConstructor_FrequencyTable_Empty()
+        {
+            FrequencyTable<int> table = new FrequencyTable<int>(this._Table);
+            Assert.AreEqual(0, table.Count);
+            Assert.AreEqual(0, table.Frequencies.Count());
+            Assert.AreEqual(0, table.Items.Count());
+        }
+
+        [TestMethod]
+        public void TestConstructor_FrequencyTable_NotEmpty()
+        {
+            int[] sample = FrequencyTableTest._Sample(1, 10, out Dictionary<int, int> dictionary);
+            FrequencyTable<int> oldTable = new FrequencyTable<int>(sample);
+            FrequencyTable<int> table = new FrequencyTable<int>(oldTable);
+            Assert.AreEqual(oldTable.Count, table.Count);
+            Assert.AreEqual(oldTable.Frequencies.Count(), table.Frequencies.Count());
+            Assert.AreEqual(oldTable.Items.Count(), table.Items.Count());
+            
+            foreach (int item in table.Items)
+            {
+                Assert.IsTrue(oldTable.Items.Contains(item));
+                Assert.AreEqual(oldTable[item], table[item]);
+            }
+            
+            foreach (int frequency in table.Frequencies)
+            {
+                Assert.IsTrue(oldTable.Frequencies.Contains(frequency));
+            }
+        }
+
+        [TestMethod]
+        public void TestConstructor_FrequencyTable_Null() => Assert.ThrowsException<ArgumentNullException>(() => new FrequencyTable<int>(null));
+#endregion
     }
 }
